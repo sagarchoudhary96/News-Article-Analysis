bdac73b462a759709552b8b4e8e9241111191d3e
Sagar Choudhary <sagar.choudhary96@gmail.com>
2019-08-24 17:45:23 +0530

    Add: test for api route /analyse

--- test/routes.test.js
+++ test/routes.test.js
@@ -5,10 +5,19 @@ const request = require("supertest");
 const app = require("../app")
 
 // home page route test
-describe("test Route: '/analyse'", () => {
+describe("test Route: '/analyse/test'", () => {
     test("Should return Hello Word!", async () => {
         const response = await request(app).get("/analyse/test")
         expect(response.text).toEqual("Hello World!")
         expect(response.statusCode).toEqual(200)
+    })
+})
+
+// api route test
+describe("test Route: '/analyse'", () => {
+    test("Should return 200 OK with response", async () => {
+        const response = await request(app).post("/analyse").send({"text": "Hello World!", "isUrl": false})
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.polarity).toEqual("neutral")
     })
 })
